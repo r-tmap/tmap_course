73da24c3a801e5db3f0c7ace49fa69c09df3088d
mtennekes <mtennekes@gmail.com>
2025-06-03 21:02:37 +0200

    ..

--- answers/answers1.R
+++ answers/answers1.R
@@ -61,3 +61,38 @@ tm3c + tm5
 tmap_mode("view")
 
 tm3c + tm5
+
+# 11. What color palette can you recommend for the following data variables in the World dataset: "well_being" , "gdp_cap_est" , and "footprint" ?
+
+#12. Make a choropleth (see 3b) of each of these variables using the selected color palettes. (Tip: put a - prefix to reverse the colors)
+
+tmap_mode("plot")
+
+tm_shape(World) +
+  tm_polygons(
+    fill = "well_being",
+    fill.scale = tm_scale_intervals(values = "-greens")) +
+tm_crs("auto")
+
+# tm_crs will be covered tomorrow
+
+# question: suppose you would like to add a highlighting color
+# how to check for color-blind-friendliness etc?
+
+# get the palette colors and add a highlighting color
+pal = c4a("greens", n = 6, reverse = TRUE)
+pal[3] = "#FF0000" # for convenience we replace the third color with red
+
+# load that palette into cols4all
+c4a_data(list(mypal = pal), types = "seq") |> 
+	c4a_load()
+
+# analyse the palette (not color-blind friendly in this case)
+c4a_gui()
+
+# create the map again
+tm_shape(World) +
+  tm_polygons(
+  fill = "well_being",
+  fill.scale = tm_scale_intervals(values = pal)) +
+tm_crs("auto")
