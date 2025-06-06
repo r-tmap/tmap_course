f0216012cd0686cff0181cd00a27cc08e950bbda
mtennekes <mtennekes@gmail.com>
2025-06-06 13:34:17 +0200

    ...

--- scripts/demo_tmap_exporting.R
+++ scripts/demo_tmap_exporting.R
@@ -25,3 +25,20 @@ tmap_save(tm, filename = "output/world_large.png", width = 2000, height = 1200, 
 tmap_save(tm, filename = "output/world_icon.png", width = 400, height = 400, scale = 0.5)
 
 tmap_save(tm, filename = "output/world.pdf", width = 1.5, height = 1.5)
+
+tm_shape(World) +
+	tm_polygons("press") +
+	tm_crs("auto") +
+	tm_layout(asp = 2)
+
+tmap_save(tm, filename = "output/world_square.pdf", width = 7, height = 7, asp = 2)
+tmap_save(tm, filename = "output/world_square2.pdf", width = 7, height = 7)
+tmap_save(tm, filename = "output/world_square3.pdf", width = 7, height = 7, asp = 0)
+
+tm = tm_shape(World) +
+	tm_polygons("press", fill.scale = tm_scale_intervals(n = 9, values = "brewer.set2")) +
+	tm_crs("auto")
+
+tmap_save(tm, filename = "output/world_srgb.pdf", width = 9, height = 6, colormodel = "srgb")
+tmap_save(tm, filename = "output/world_cmyk.pdf", width = 9, height = 6, colormodel = "cmyk")
+
